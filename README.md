aa0ddc244ee2a45db5aeac463a5d7b8f1b4629ee
Patrick Gebert <patrick.gebert@web.de>
2023-11-18 14:13:06 +0100

    Covering all basic terraform commands.

--- README.md
+++ README.md
@@ -57,14 +57,42 @@ New-Item -ItemType File $profile
 
 ## 🚀 Supported Aliases
 
-| Alias | Command              |
-| ----- | -------------------- |
-| `tf`  | `terraform`          |
-| `tfa` | `terraform apply`    |
-| `tfc` | `terraform console`  |
-| `tfd` | `terraform destroy`  |
-| `tff` | `terraform fmt`      |
-| `tfi` | `terraform init`     |
-| `tfo` | `terraform output`   |
-| `tfp` | `terraform plan`     |
-| `tfv` | `terraform validate` |
+| Alias   | Command                           |
+|---------|-----------------------------------|
+| `tf`    | `terraform`                       |
+| `tfa`   | `terraform apply`                 |
+| `tfc`   | `terraform console`               |
+| `tfd`   | `terraform destroy`               |
+| `tff`   | `terraform fmt`                   |
+| `tfg`   | `terraform graph`                 |
+| `tfim`  | `terraform import`                |
+| `tfi`   | `terraform init`                  |
+| `tfo`   | `terraform output`                |
+| `tfp`   | `terraform plan`                  |
+| `tfpr`  | `terraform providers`             |
+| `tfr`   | `terraform refresh`               |
+| `tfsh`  | `terraform show`                  |
+| `tft`   | `terraform taint`                 |
+| `tfut`  | `terraform untaint`               |
+| `tfv`   | `terraform validate`              |
+| `tfw`   | `terraform workspace`             |
+| `tfs`   | `terraform state`                 |
+| `tffu`  | `terraform force-unlock`          |
+| `tfwst` | `terraform workspace select`      |
+| `tfwsw` | `terraform workspace show`        |
+| `tfssw` | `terraform state show`            |
+| `tfwde` | `terraform workspace delete`      |
+| `tfwls` | `terraform workspace list`        |
+| `tfsls` | `terraform state list`            |
+| `tfwnw` | `terraform workspace new`         |
+| `tfsmv` | `terraform state mv`              |
+| `tfspl` | `terraform state pull`            |
+| `tfsph` | `terraform state push`            |
+| `tfsrm` | `terraform state rm`              |
+| `tfay`  | `terraform apply -auto-approve`   |
+| `tfdy`  | `terraform destroy -auto-approve` |
+| `tfinu` | `terraform init -upgrade`         |
+| `tfpde` | `terraform plan --destroy`        |
+
+
+terraform state
--- src/aliases.ps1
+++ src/aliases.ps1
@@ -6,10 +6,37 @@ Remove-Alias tfa -Force -ErrorAction SilentlyContinue
 Remove-Alias tfc -Force -ErrorAction SilentlyContinue
 Remove-Alias tfd -Force -ErrorAction SilentlyContinue
 Remove-Alias tff -Force -ErrorAction SilentlyContinue
+Remove-Alias tfg -Force -ErrorAction SilentlyContinue
+Remove-Alias tfim -Force -ErrorAction SilentlyContinue
 Remove-Alias tfi -Force -ErrorAction SilentlyContinue
 Remove-Alias tfo -Force -ErrorAction SilentlyContinue
 Remove-Alias tfp -Force -ErrorAction SilentlyContinue
+Remove-Alias tfpr -Force -ErrorAction SilentlyContinue
+Remove-Alias tfr -Force -ErrorAction SilentlyContinue
+Remove-Alias tfsh -Force -ErrorAction SilentlyContinue
+Remove-Alias tft -Force -ErrorAction SilentlyContinue
+Remove-Alias tfut -Force -ErrorAction SilentlyContinue
 Remove-Alias tfv -Force -ErrorAction SilentlyContinue
+Remove-Alias tfw -Force -ErrorAction SilentlyContinue
+Remove-Alias tfwl -Force -ErrorAction SilentlyContinue
+Remove-Alias tfws -Force -ErrorAction SilentlyContinue
+Remove-Alias tfs -Force -ErrorAction SilentlyContinue
+Remove-Alias tffu -Force -ErrorAction SilentlyContinue
+Remove-Alias tfwst -Force -ErrorAction SilentlyContinue
+Remove-Alias tfwsw -Force -ErrorAction SilentlyContinue
+Remove-Alias tfssw -Force -ErrorAction SilentlyContinue
+Remove-Alias tfwde -Force -ErrorAction SilentlyContinue
+Remove-Alias tfwls -Force -ErrorAction SilentlyContinue
+Remove-Alias tfsls -Force -ErrorAction SilentlyContinue
+Remove-Alias tfwnw -Force -ErrorAction SilentlyContinue
+Remove-Alias tfsmv -Force -ErrorAction SilentlyContinue
+Remove-Alias tfspl -Force -ErrorAction SilentlyContinue
+Remove-Alias tfsph -Force -ErrorAction SilentlyContinue
+Remove-Alias tfsrm -Force -ErrorAction SilentlyContinue
+Remove-Alias tfay -Force -ErrorAction SilentlyContinue
+Remove-Alias tfdy -Force -ErrorAction SilentlyContinue
+Remove-Alias tfinu -Force -ErrorAction SilentlyContinue
+Remove-Alias tfpde -Force -ErrorAction SilentlyContinue
 
 function tf {
 	terraform $args
@@ -30,6 +57,14 @@ function tff {
 	terraform fmt $args
 }
 
+function tfg {
+	terraform graph $args
+}
+
+function tfim {
+	terraform import $args
+}
+
 function tfi {
 	terraform init $args
 }
@@ -42,6 +77,98 @@ function tfp {
 	terraform plan $args
 }
 
+function tfpr {
+	terraform providers $args
+}
+
+function tfr {
+	terraform providers $args
+}
+
+function tfsh {
+	terraform show $args
+}
+
+function tft {
+	terraform taint $args
+}
+
+function tfut {
+	terraform untaint $args
+}
+
 function tfv {
 	terraform validate $args
+}
+
+function tfw {
+	terraform workspace $args
+}
+
+function tfs {
+	terraform state $args
+}
+
+function tffu {
+	terraform force-unlock $args
+}
+
+function tfwst {
+	terraform workspace select $args
+}
+
+function tfwsw {
+	terraform workspace show $args
+}
+
+function tfssw {
+	terraform state show $args
+}
+
+function tfwde {
+	terraform workspace delete $args
+}
+
+function tfwls {
+	terraform workspace list $args
+}
+
+function tfsls {
+	terraform state list $args
+}
+
+function tfwnw {
+	terraform workspace new $args
+}
+
+function tfsmv {
+	terraform state mv $args
+}
+
+function tfspl {
+	terraform state pull $args
+}
+
+function tfsph {
+	terraform state push $args
+}
+
+function tfsrm {
+	terraform state rm $args
+}
+
+function tfay {
+	terraform apply -auto-approve $args
+}
+
+function tfdy {
+	terraform destroy -auto-approve $args
+}
+
+function tfinu {
+	terraform init -upgrade $args
+}
+
+function tfpde {
+	terraform plan --destroy $args
 }
